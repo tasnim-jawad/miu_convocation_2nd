08f1a57a541c914533012281deb5aa2b58941dd7
tasniul-hasan <tasnimulh453@gmail.com>
2023-07-26 21:47:56 +0600

    nothing

--- js/script.js
+++ js/script.js
@@ -177,8 +177,8 @@ document.addEventListener('DOMContentLoaded', function() {
   
   // Placeholder variable for PDF file paths
   var pdfFiles = {
-    'Notice 2': 'images/Notice/notice2.pdf',
-    'Notice 1': 'images/Notice/notice1.pdf',
+    'Notice 2': '../images/Notice/notice2.pdf',
+    'Notice 1': '../images/Notice/notice1.pdf',
     // Add more notice titles and their corresponding PDF file paths
   };
 
